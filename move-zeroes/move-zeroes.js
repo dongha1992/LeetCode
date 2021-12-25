1712c2f8fb2080a10a2c0a6b83d10ed95f78edd7
김동하 <58624851+dongha1992@users.noreply.github.com>
2021-12-25 17:17:48 +0900

    Time: 80 ms (99.97%), Space: 43.7 MB (50.13%) - LeetHub

---
+++ move-zeroes/move-zeroes.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+  
+let idx = 0;
+  
+ for(let i = 0; i < nums.length; i++){
+   if(nums[i]){
+     nums[idx++] = nums[i]
+   }
+ }
+for(let i = idx; i <nums.length; i++){
+  nums[i]= 0
+}
+  
+};
