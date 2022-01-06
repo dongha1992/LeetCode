ccf6f6d26df3067fcbeabcb620ac3ed64c531805
김동하 <58624851+dongha1992@users.noreply.github.com>
2022-01-06 20:42:51 +0900

    Time: 89 ms (21.58%), Space: 38.8 MB (73.62%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.js
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.js
@@ -3,7 +3,5 @@
  * @return {string}
  */
 var interpret = function(command) {
-
-  return command.replace(/\(\)/g,'o').replace(/\(al\)/g, 'al')
-
+return command.split("()").join("o").split("(al)").join("al");
 };
