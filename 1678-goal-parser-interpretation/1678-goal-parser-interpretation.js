e2cdc5dd9048db5b02d488e400a8bdba324abc2f
김동하 <58624851+dongha1992@users.noreply.github.com>
2022-01-06 20:41:53 +0900

    Time: 136 ms (5.28%), Space: 38.2 MB (98.56%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.js
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.js
@@ -3,20 +3,7 @@
  * @return {string}
  */
 var interpret = function(command) {
-const parsed = [];
-for (let i = 0; i < command.length; i++) {
-  const isO = command[i] === '(' && command[i + 1] === ')';
-  const isAl = command[i] === '(' && command[i + 1] !== ')';
-  if (isO) {
-    parsed.push('o');
-    i = i + 1;
-  } else if (isAl) {
-    parsed.push('al');
-    i = i + 3;
-  } else {
-    parsed.push('G');
-  }
-}
-return parsed.join('');
+
+  return command.replace(/\(\)/g,'o').replace(/\(al\)/g, 'al')
 
 };
