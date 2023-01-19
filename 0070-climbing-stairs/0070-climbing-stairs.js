d1fb2239e064cdfec6357cdbc7429a1d8265352b
김동하 <58624851+dongha1992@users.noreply.github.com>
2023-01-19 16:22:24 +0900

    Time: 62 ms (81.16%), Space: 41.2 MB (97.02%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    const dy = Array.from({length:n+1}, ()=>0)
+    
+    
+    dy[1] = 1
+    dy[2] = 2
+    for(let i = 3; i<= n; i++){
+      dy[i] = dy[i-1] + dy[i-2]
+    }
+    return dy[n]
+};
