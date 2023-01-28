54cf2bb5f1425516c5b591cf5cbc68fdba20c9fc
김동하 <58624851+dongha1992@users.noreply.github.com>
2023-01-28 21:13:04 +0900

    Time: 92 ms (83.11%), Space: 46.8 MB (43.29%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.js
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -3,12 +3,26 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
-    for(let i = 0; i < nums.length-1; i++){
-      for(let j = 0; j < nums.length-1-i; j++){
-        if(nums[j] === 0){
-          [nums[j], nums[j+1]] = [nums[j+1],nums[j]]
-        }
+    
+    /* 버블정렬로 755ms */
+    
+    // for(let i = 0; i < nums.length-1; i++){
+    //   for(let j = 0; j < nums.length-1-i; j++){
+    //     if(nums[j] === 0){
+    //       [nums[j], nums[j+1]] = [nums[j+1],nums[j]]
+    //     }
+    //   }
+    // }
+    
+    /* 투 포인터 */
+    
+    let j = 0;
+    for(let i = 0; i < nums.length; i++){
+      if(nums[i]!==0){
+        [nums[i],nums[j]] = [nums[j], nums[i]]
+        j++
       }
     }
+    
   return nums
 };
