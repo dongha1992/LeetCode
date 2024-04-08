9aeefea222357d7f50f8bd2efbe9927f800ec6ba
김동하 <58624851+dongha1992@users.noreply.github.com>
2024-04-09 00:45:15 +0900

    Time: 58 ms (64.04%), Space: 52.2 MB (21.76%) - LeetHub

---
+++ 0206-reverse-linked-list/0206-reverse-linked-list.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+
+
+
+var reverseList = function(head) {
+   let previous = null;
+   let current = head
+    while(current){
+      let next = current.next;
+      current.next = previous;
+      previous = current
+      current = next
+    
+    }
+    return previous
+};
