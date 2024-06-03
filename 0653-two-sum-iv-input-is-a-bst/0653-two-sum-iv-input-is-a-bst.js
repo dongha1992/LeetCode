e7e90d8458607df9a41e92ea3b8361131e6f47fa
김동하 <58624851+dongha1992@users.noreply.github.com>
2024-06-03 12:54:31 +0900

    Time: 80 ms (51.74%), Space: 59.3 MB (28.11%) - LeetHub

---
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {boolean}
+ */
+var findTarget = function(root, k) {
+    const set = new Set()
+    let answer = false
+    const dfs = (node) => {
+        if(!node) return
+        if(set.has(k - node.val)) {
+         answer = true
+         return
+        }
+        set.add(node.val)
+        dfs(node.left) 
+        dfs(node.right)
+    }
+    dfs(root)
+    return answer
+};
