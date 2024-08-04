920b5b01430836823f6735163e7df00c790cc5ee
김동하 <58624851+dongha1992@users.noreply.github.com>
2024-08-04 15:13:17 +0900

    Time: 1 ms (13.71%), Space: 42.5 MB (6.30%) - LeetHub

---
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+import java.util.LinkedList;
+import java.util.Queue; 
+
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) return true;
+        
+        Queue<TreeNode> queue = new LinkedList<>(); 
+        queue.add(root);
+        queue.add(root);
+        
+        while(!queue.isEmpty()){
+            TreeNode left = queue.poll();
+            TreeNode right = queue.poll();
+            
+            if (left == null && right == null) continue;
+            if (left == null || right == null) return false;
+            if (left.val != right.val) return false;
+            
+            queue.add(left.left);
+            queue.add(right.right);
+            queue.add(left.right);
+            queue.add(right.left);
+        }
+        return true;
+    }
+}
